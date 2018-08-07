05f366aacce247fab361dfa0af58aaf61e102517
김선정 <lamoshalt811@gmail.com>
2018-08-07 00:36:48 +0000

    solve problems

--- 1.step/2.arithmetic/[2839]sugar/sugar.cpp
+++ 1.step/2.arithmetic/[2839]sugar/sugar.cpp
@@ -1,29 +1,31 @@
-#include <stdio.h>
+	#include <stdio.h>
 
-int arr[5001]
+	int arr[5001];
 
-int main() {
-	
-	int i, N;
-	
-	scanf("%d", &N);
-	
-	arr[3] = 1;
-	arr[4] = -1;
-	arr[5] = 1;
-	for(i=6; i<=n; i++) {
-		if(arr[i-3] > 0 && arr[i-5] > 0) {
-			arr[i] = 1 + (arr[i-3] < arr[i-5]) ? arr[i-3] : arr[i-5];
-		} else if(arr[i-3] > 0) {
-			arr[i] = 1 + arr[i-3];
-		} else if(arr[i-5] > 0) {
-			arr[i] = 1 + arr[i-5];
-		} else {
-			arr[i] = -1;
-		}		
+	int main() {
+
+		int i, N;
+
+		scanf("%d", &N);
+
+		arr[1] = -1;
+		arr[2] = -1;
+		arr[3] = 1;
+		arr[4] = -1;
+		arr[5] = 1;
+		for(i=6; i<=N; i++) {
+			if(arr[i-3] > 0 && arr[i-5] > 0) {
+				arr[i] = 1 + ((arr[i-3] < arr[i-5]) ? arr[i-3] : arr[i-5]);
+			} else if(arr[i-3] > 0) {
+				arr[i] = 1 + arr[i-3];
+			} else if(arr[i-5] > 0) {
+				arr[i] = 1 + arr[i-5];
+			} else {
+				arr[i] = -1;
+			}		
+		}
+
+		printf("%d\n", arr[N]);
+
+		return 0;
 	}
-	
-	printf("%d\n", arr[N]);
-	
-	return 0;
-}
---
+++ 1.step/7.string/[11654]ascii/ascii.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	
+	char s = getchar();
+	printf("%d\n", s);
+	
+	return 0;
+}
